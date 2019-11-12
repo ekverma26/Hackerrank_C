9715d6139e644f64c8d1efdafcec4df24dcbd044
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-12 23:10:23 +0530

    Create Playing_with_characters.c

---
+++ Introduction/Playing_with_characters.c
@@ -0,0 +1,49 @@
+Task
+
+You have to print the character, , in the first line. Then print  in next line. In the last line print the sentence, .
+
+Input Format
+
+First, take a character,  as input.
+Then take the string,  as input.
+Lastly, take the sentence  as input.
+
+Output Format
+
+Print three lines of output. The first line prints the character, .
+The second line prints the string, .
+The third line prints the sentence, .
+
+Sample Input 0
+C
+Language
+Welcome To C!!
+
+Sample Output 0
+C
+Language
+Welcome To C!!
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    char ch;
+    char s[20], sen[100];
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+    return 0;
+
+}
+
