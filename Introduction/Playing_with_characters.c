f9fecef0cabc2951f2c2ddff92362b31607ffa25
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-12 23:11:09 +0530

    Update Playing_with_characters.c
    Task
    
    You have to print the character, , in the first line. Then print  in next line. In the last line print the sentence, .
    
    Input Format
    
    First, take a character,  as input.
    Then take the string,  as input.
    Lastly, take the sentence  as input.
    
    Output Format
    
    Print three lines of output. The first line prints the character, .
    The second line prints the string, .
    The third line prints the sentence, .
    
    Sample Input 0
    C
    Language
    Welcome To C!!
    
    Sample Output 0
    C
    Language
    Welcome To C!!

--- Introduction/Playing_with_characters.c
+++ Introduction/Playing_with_characters.c
@@ -1,29 +1,3 @@
-Task
-
-You have to print the character, , in the first line. Then print  in next line. In the last line print the sentence, .
-
-Input Format
-
-First, take a character,  as input.
-Then take the string,  as input.
-Lastly, take the sentence  as input.
-
-Output Format
-
-Print three lines of output. The first line prints the character, .
-The second line prints the string, .
-The third line prints the sentence, .
-
-Sample Input 0
-C
-Language
-Welcome To C!!
-
-Sample Output 0
-C
-Language
-Welcome To C!!
-
 
 #include <stdio.h>
 #include <string.h>
